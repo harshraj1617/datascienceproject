e070d6903bffd4804f6c14a9a0ed8a2ce1b40a25
harshraj1617 <rajsinghharsh1611@gmail.com>
2026-01-11 14:19:18 +0530

    data validation completed

---
+++ src/datascience/pipeline/data_validation_pipeline.py
@@ -0,0 +1,27 @@
+from src.datascience.config.configuration import ConfigurationManager
+from src.datascience.components.data_validation import DataValidation
+from src.datascience import logger
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_validation(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+"""This main function is use to test the data validation pipeline independently."""
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
